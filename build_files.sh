8e152365cbe700501e0720c128d9891efb973ba0
plabondatta26 <plabondatta26@gmail.com>
2023-02-21 23:59:53 +0600

    vercel configured

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "Build Start"
-python3.7 -m pip install -r requirements.txt
-python3.7 -m pip install -r migrate
-python3.7 -m pip install -r collectstatic --noiput --clear
+python3.6 -m pip install -r requirements.txt
+python3.6 -m pip install -r migrate
+python3.6 -m pip install -r collectstatic --noiput --clear
 echo "Build End"
